1a2caf66397ce850980ae1b3c829ca12fb58f470
Seth Bryant <bryant.seth@gmail.com>
2023-12-04 14:37:34 -0700

    add eval tests

---
+++ parameters.py
@@ -0,0 +1,7 @@
+import os
+
+src_dir = os.path.dirname(os.path.abspath(__file__))
+src_name = os.path.basename(src_dir)
+
+#location of logging configuration file
+logcfg_file=os.path.join(src_dir, r'hp\logger.conf')
